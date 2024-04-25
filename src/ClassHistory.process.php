8b98dc946916a67128f194f8f7be947902a94734
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2024-04-25 01:40:11 -0400

    Update: class history pdf parsing via pdfparser, data validation
    Foundations of error message & redirect for nonexistent data fields within class history pdf, extraction of text from pdf files complete, various FIXMEs for html content and completion of field extraction for insertion into database

--- src/ClassHistory.process.php
+++ src/ClassHistory.process.php
@@ -3,12 +3,48 @@
  * Contains methods to read ClassHistory pdf file and turn into database entry
  */
 
+
+/**
+ * Ends execution and notifies user of data missing from class history document, sending back to ClassHistoryForm.html for reexecution
+ * @see ClassHistoryForm.hmtl
+ *
+ * @param	String	$missing_data	specifies the field name for missing data
+ * @return void
+ */
+function missingData(string $missing_data, string $error_text = "%s information cannot be found within the class history document.") {
+	echo "<p>"; // FIXME: style tags to center text block in middle of screen
+	echo sprintf($error_text, $missing_data) . " Please re-upload valid class history document.";
+	echo "</p>"; // FIXME: button leading to ClassHistoryForm.html
+	die();
+}
+
+/** 
+ * I: Parses PDF file and return content as String array
+ * 
+ * @see https://github.com/smalot/pdfparser/blob/master/doc/Usage.md
+ */
 include 'pdfparser.php-dist';
 
-// Parse PDF file and return content as String
 $parser = new \Smalot\PdfParser\Parser();
 $pdf = $parser->parseFile('Class History.pdf');
 
-$text = $pdf->getText();;
-echo $text;
+// places text data from all pages of class history document into flat $text array
+$text = [];
+foreach ($pdf->getPages() as $page) {
+	$text = array_merge($text, $page->getTextArray());
+}
+
+// notify user if document does not contain "Class History"
+if (!in_array("Class History", $text)) {
+	missingData('PDF uploaded as class history is missing "Class History" header', "%s. This document is not a class history document.");
+}
+
+
+/**
+ * II: Assigns user data (UD) for database allocation
+ */
+
+echo "<pre>";
+print_r($text);
+echo "</pre>";
 ?>
