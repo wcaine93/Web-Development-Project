fce3e523cd46b1502b7b67f65d748226767f8b7d
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2024-05-04 16:51:38 -0400

    Create HasError.php

---
+++ src/HasError.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Driver class for ErrorPage.html, collects errors for display on error page to user
+ *
+ * @see ErrorPage.html
+ */
+
+$page_name = __FILE__; // default page name to file name
+$errors = [];
+
+/**
+ * Adds error text to $errors array
+ *
+ * @param	String	$error_text
+ * @return	void
+ */
+function error($error_text) {
+	$errors[] = $error_text;
+}
+
+/**
+ * Set page name for display on error page
+ *
+ * @return	void
+ */
+function page($name) {
+	$page_name = 'the' . $name;
+}
+
+/**
+ * Displays error page using $errors array
+ *
+ * @return	void
+ */
+function display() {
+	readfile('ErrorPage.html');
+}
+?>
